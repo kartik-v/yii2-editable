eddb718d49fe4b21f9b50457066a4091b5a72a4a
Kartik Visweswaran <kartikv2@gmail.com>
2015-03-29 19:40:03 +0530

    Fix #54: Auto guess input and set input options based on input type

--- Editable.php
+++ Editable.php
@@ -201,6 +201,16 @@ class Editable extends InputWidget
      * set to [[Editable::INPUT_WIDGET]]
      */
     public $widgetClass;
+    
+    /**
+     * @var boolean whether to auto guess the input type and set the editable input options. When 
+     * this is set to `true` the Editable widget will automatically detect the core input options
+     * for common input types (including HTML input types, krajee widgets and DateControl) and append 
+     * the `kv-editable-input` class to the input. You can set this to false to set the `kv-editable-input`
+     * class manually for any other custom/third party widgets. You would do this typically when you set 
+     * inputType to `Editable::INPUT_WIDGET`. Defaults to `true`.
+     */
+    public $autoGuessInput = true;
 
     /**
      * @var boolean additional ajax settings to pass to the plugin.
@@ -258,16 +268,6 @@ class Editable extends InputWidget
      * - $widget mixed is the current editable widget instance
      */
     public $afterInput;
-    
-    /**
-     * @var boolean whether to auto guess the input type and set the editable input options. When 
-     * this is set to `true` the Editable widget will automatically detect the core input options
-     * for common input types (including HTML input types, krajee widgets and DateControl) and append 
-     * the `kv-editable-input` class to the input. You can set this to false to set the `kv-editable-input`
-     * class manually for any other custom/third party widgets. You would do this typically when you set 
-     * inputType to `Editable::INPUT_WIDGET`. Defaults to `true`.
-     */
-    public $autoGuessInput = true;
 
     /**
      * @var boolean whether you wish to automatically display the form submit and reset buttons.
